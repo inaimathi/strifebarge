f8e73d5007310c1a15f1bafc7c06843936a55fff
inaimathi <inaimathi@hermaeus.gateway.2wire.net>
2012-03-03 01:39:45 -0500

    -Trimming useless code and reordering js-macros

--- history-event.lisp
+++ history-event.lisp
@@ -24,13 +24,3 @@
 (defmethod emit-record ((e history-event) (p player))
   (format nil "id: ~a~%event: ~a~%data: ~a~%~%"
 	  (id e) (event-type e) (message e)))
-
-(defmethod emit-record ((m hit) (p player))
-  (format nil "event: shot~%data: ~a~%~%event: turn~%data: ~a~%~%"
-	  (encode-json-to-string `((x . ,(x m)) (y . ,(y m)) (text . ,(echo m p))))
-	  (if (eq (player m) p) "Their Turn" "Your Turn")))
-
-(defmethod emit-record ((m miss) (p player))
-  (format nil "event: shot~%data: ~a~%~%event: turn~%data: ~a~%~%"
-	  (encode-json-to-string `((x . ,(x m)) (y . ,(y m)) (text . ,(echo m p))))
-	  (if (eq (player m) p) "Their Turn" "Your Turn")))
--- js-macros.lisp
+++ js-macros.lisp
@@ -17,16 +17,6 @@
 (defpsmacro parse-json (target)
   `(chain j-query (parse-j-s-o-n ,target)))
 
-(defpsmacro define-event-source (name source-url)
-  `(defvar ,name (new (-event-source ,source-url))))
-
-(defpsmacro define-event-listener (event-source-name message-type on-receive)
-  "event-source-name must be a defined event source. 
-message-type is the event label sent by the server for this action (the default label is 'message').
-on-receive is a function called when a satisfying message is received. It should take one argument (the event)."
-  `(chain ,event-source-name (add-event-listener ,message-type ,on-receive false)))
-
-;;;;;;;;;;;;;;; clomment specific
 (defpsmacro post-to (target-page data-hash on-success) ; data hash declared like (create :k v ...)
   "target-page is a page url.
 data-hash is the data sent along as the post request; declared as (create :k v ...)
@@ -36,10 +26,16 @@ it should expect a single argument (the data returned by the target handler)"
 		  ,data-hash
 		  ,on-success)))
 
+;;;;;;;;;;;;;;; SSE specific
+(defpsmacro define-event-source (name source-url)
+  `(defvar ,name (new (-event-source ,source-url))))
+
+(defpsmacro define-event-listener (event-source-name message-type on-receive)
+  "event-source-name must be a defined event source. 
+message-type is the event label sent by the server for this action (the default label is 'message').
+on-receive is a function called when a satisfying message is received. It should take one argument (the event)."
+  `(chain ,event-source-name (add-event-listener ,message-type ,on-receive false)))
+
+;;;;;;;;;;;;;;; StrifeBarge specific
 (defpsmacro $-space-at ((x y) &rest chains)
   `($ "#game-board tr" (eq ,y) (children "td") (eq ,x) ,@chains))
-
-(defpsmacro send-shot (x y)
-  `(post-to "/turn" (create :x ,x :y ,y) 
-	    (lambda (data)
-	      ($-space-at (,x ,y) (text data)))))
